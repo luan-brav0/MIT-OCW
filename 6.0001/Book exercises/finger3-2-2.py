86c048c3a8c7e97dd14683865d7046047db56dda
LuanBrav0 <contatotropicaldesign@gmail.com>
2022-08-31 18:38:32 -0300

    small fixes

--- 6.0001/Book exercises/finger3-2-2.py
+++ 6.0001/Book exercises/finger3-2-2.py
@@ -1,3 +1,4 @@
+import timeit
 # list = []
 # print(len(list))
 # l=0
@@ -13,21 +14,24 @@
 # print(list[l])
 
 s = '1.23,2.4,3.123'
+def f1(s):
+    ans = sum(map(float, s.split(",")))
+    print (ans)
+    return
+def f2(s):
+    for d in range(len(s)):
+        if s[d] != ',': 
+            num += s[d]
+        else: 
+            ans += float(num)
+            num = ''
+    print(ans)
+    return
 
-# s = s.split(',')
-# ans = 0
-# for n in range(len(s)):
-#     ans +=  float(s[n])
-#     print (ans)
-ans = 0
-num = ''
-for d in range(len(s)):
-    if s[d] != ',': 
-        num += s[d]
-    else: 
-        ans += float(num)
-        num = ''
-print(ans)
+    
+timeit.timeit("f1(s)", setup="from __main__ import f1, f2;s='1.23,2.4,3.123'")
+
+
 
         
 
--- 6.0001/Book exercises/finger3-2.py
+++ 6.0001/Book exercises/finger3-2.py
@@ -1,5 +1,18 @@
+
 # Devide a series of floats in a string separated by commas present the total sum.
 
 #given string
 s = '1.23,2.4,3.123'
 
+ans = 0
+num = ''
+
+for d in range(len(s)):
+    if s[d] != ',': 
+        num += s[d]
+    else: 
+        ans += float(num)
+        num = ''
+# alternatively: 
+# ans = sum(map(s.split(',')))
+print(ans)
