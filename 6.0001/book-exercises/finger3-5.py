0ee9652876e6a78a0c44daf69d00c1cea560ec4b
LuanBrav0 <contatotropicaldesign@gmail.com>
2022-09-07 11:05:16 -0300

    finger 3-5 done

--- 6.0001/book-exercises/finger3-5.py
+++ 6.0001/book-exercises/finger3-5.py
@@ -1,51 +1,49 @@
 # Newton-Raphson for square root 
 # Find x such that x**2 - 24 is within epsilon of 0
-from doctest import NORMALIZE_WHITESPACE
-from mimetypes import init
-
 
 eps = 0.01
 # initial given value
-initVal = 24.0
-# current guess number
+initVal = 25.0
 
 
 
 def newt(eps, initVal, root): 
+    initVal = float(initVal)
     guess = initVal/2.0
     TotalGuesses = 0
-    while abs(guess**root - initVal) >= eps:
+    while abs(abs(guess)**root- abs(initVal)) >= eps:
         TotalGuesses += 1
-        guess = guess - (((guess**2) - initVal)/(2*guess))
+        guess = guess - (((guess**root) - initVal)/(root*guess**(root-1)))
 
     print (f'Total guesses (Newton-Raphson): {TotalGuesses}')
-    print (f'The square root of {initVal} is about {guess}')
-    return guess
+    print (f'The square root of {initVal} is about {round(guess,len(str(eps)))}')
+    return TotalGuesses
 
 def bisec(eps, initVal, root):
     high = max(1.0, initVal) if initVal > 0 else min(-1.0, initVal)
     low = 0.0
-    ans = (high + low)/2.0
+    guess = (high + low)/2.0
     totalGuesses = 0
 
-    # while the difference of initVal and cubed ans isn't eps or bigger 
-    while abs(abs(ans)**root - abs(initVal)) >= eps:
-        print(low, high, ans)
-        guesses += 1
-        # if cubed ans is bigger than initVal, low should be current value of ans, else high takes the value 
-        if abs(ans**3) < abs(initVal): 
-            low = ans 
+    # while the difference of initVal and cubed guess isn't eps or bigger 
+    while abs(abs(guess)**root - abs(initVal)) >= eps:
+        
+        totalGuesses += 1
+        # if cubed guess is bigger than initVal, low should be current value of guess, else high takes the value 
+        if abs(guess**root) < abs(initVal): 
+            low = guess 
         else: 
-            high = ans
-        # updates ans
-        ans = (high + low)/2.0
+            high = guess
+        # updates guess
+        guess = (high + low)/2.0
 
-    # prints total guesses and answer
-    print ('Total guesses (bisectional): ', guesses)
-    print (ans, ' is close to sqrt of ', initVal)
-    return ans
+    # prints total guesses and guesswer
+    print (f'Total guesses (bisectional): {totalGuesses}')
+    print (f'{round(guess,len(str(eps)))} is close to sqrt of {initVal}')
+    return totalGuesses
 
-def compare(bisec,newt):
-    eval(bisec)
-    eval(newt)
-    return bisec/newt
+
+newt(eps, initVal, 2)
+bisec(eps, initVal, 2)
+
+print(f"Newton's method is {(bisec(eps, initVal, 2) / newt(eps, initVal, 2) )} times faster")
