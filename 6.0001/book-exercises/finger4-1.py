54ad1fa8cc2128d1dca5bd048eef7f2ddad32678
LuanBrav0 <contatotropicaldesign@gmail.com>
2022-09-08 10:39:16 -0300

    self-challenge finger4-1 done

--- 6.0001/book-exercises/finger4-1.py
+++ 6.0001/book-exercises/finger4-1.py
@@ -13,14 +13,15 @@ text = 'def'
 # self-challenge: not only check if it is in that, but tell the starting and ending index where s2 matches in s1
 
 
-def isIn(s1, s2):
-    if s2 in s1:
+
+def matchStrings(s1,s2):
+    def isIn(s1, s2):
+        return s2 in s1
+    if isIn:
         for d in range(len(s1)):
             if s1[d]  == s2[0] and s1[d+len(s2)-1] == s2[-1]:
                 print(f'The second string matches at indexes {d} to {d+len(s2)-1}')
-                if s2 in s1: return True 
-                else: return False
             
-            
+
     
-isIn(texto, text)
+matchStrings(texto, text)
